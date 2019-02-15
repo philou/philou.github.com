ee2abddd7753a7321692e05f8c4d5659e274c8c7
Philippe Bourgau <philippe.bourgau@gmail.com>
2019-02-15 12:56:25 +0100

    Better color for bad links detector

--- _check_links.sh
+++ _check_links.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
+RED="\033[31m"
 YELLOW="\033[33m"
 NORMAL="\033[0;39m"
 
-echo $YELLOW
-
-echo "Checking for absolute links"
+echo "Checking for absolute links ${RED}"
 find _posts/ -type f -and -name "*.markdown" -and -exec grep -H "philippe.bourgau.net" {} \; | grep -v posterous_url | sort
 find _posts/ -type f -and -name "*.markdown" -and -exec grep -H "127.0.0.1" {} \; | sort
+echo $NORMAL
 
-echo
-echo "Checking for links to email feed"
+echo "Checking for links to email feed ${YELLOW}"
 find _posts/ -type f -and -name "*.markdown" -and -exec grep -H "dxKE95" {} \; | sort
-
 echo $NORMAL
