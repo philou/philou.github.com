5cefabb6e97f06fff3a295d9de99182f7cbc0ea2
Philippe Bourgau <philippe.bourgau@gmail.com>
2019-03-11 09:03:38 +0100

    Check for ../imgs links

--- _check_links.sh
+++ _check_links.sh
@@ -4,9 +4,10 @@ RED="\033[31m"
 YELLOW="\033[33m"
 NORMAL="\033[0;39m"
 
-echo "Checking for absolute links ${RED}"
+echo "Checking for links to replace with {{site.url}} ${RED}"
 find _posts/ -type f -and -name "*.markdown" -and -exec grep -H "philippe.bourgau.net" {} \; | grep -v posterous_url | sort
 find _posts/ -type f -and -name "*.markdown" -and -exec grep -H "127.0.0.1" {} \; | sort
+find _posts/ -type f -and -name "*.markdown" -and -exec grep -H "\.\./imgs" {} \; | sort
 echo $NORMAL
 
 echo "Checking for links to email feed ${YELLOW}"
