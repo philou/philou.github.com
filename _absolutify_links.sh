4fa565e810f71a5e1b79428e41935f66e31540a6
Philippe Bourgau <philippe.bourgau@gmail.com>
2020-03-18 09:16:04 +0100

    Make absolutify_links script work on a single file
    To avoid unwanted changes in existing and future posts, for example in code
    - add a usage section
    - take 1 parameter as post file path
    - add to git before change
    - git diff after to change for review

--- _absolutify_links.sh
+++ _absolutify_links.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
+export POST_FILE_PATH=$1
+
+# Display usage if not called correctly
+if [[ $# -ne 1 ]] ; then
+    echo "Usage ./_absolutify_links.sh POST_FILE_PATH"
+    exit 1
+fi
+
+echo "Adding current version to git"
+git add $POST_FILE_PATH
+
 echo "Prefixing image links with {{site.url}}"
-find _posts/ -type f -and -name "*.markdown" -and -exec sed -i '' 's:(\.\./imgs:({{site.url}}/imgs:g' {} \;
+sed -i '' 's:(\.\./imgs:({{site.url}}/imgs:g' $POST_FILE_PATH
 
 echo "Prefixing site links with {{site.url}}"
-find _posts/ -type f -and -name "*.markdown" -and -exec sed -i '' 's:(/:({{site.url}}/:g' {} \;
+sed -i '' 's:(/:({{site.url}}/:g' $POST_FILE_PATH
+
+echo ''
+git diff $POST_FILE_PATH
