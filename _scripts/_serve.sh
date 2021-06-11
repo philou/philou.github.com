f433abde762f04b151545227174d02a40638401b
Philippe Bourgau <philippe.bourgau@gmail.com>
2021-06-11 18:29:28 +0200

    Delete the _scripts

--- _scripts/_serve.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-BASEDIR=$(dirname "$0")
-
-"$BASEDIR"/_check_fixmes.sh
-"$BASEDIR"/_check_links.sh
-
-bundle exec jekyll clean
-bundle exec jekyll serve --incremental  --port 5000 --unpublished --future --safe "$@"
